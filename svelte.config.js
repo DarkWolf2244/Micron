d5600e8507f575a136166f042ea27c3d9ba2cb06
DarkWolf2244 <darkwolfx2244@gmail.com>
2026-04-28 23:18:51 +0530

    ci: deploy to github pages

--- svelte.config.js
+++ svelte.config.js
@@ -4,9 +4,17 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	compilerOptions: {
 		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-		runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true
+		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
 	},
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined, // SPA fallback file (e.g., '200.html' or 'index.html')
+			precompress: false, // Generate .gz and .br compressed versions
+			strict: true // Fail build if any page can't be prerendered
+		})
+	}
 };
 
 export default config;
